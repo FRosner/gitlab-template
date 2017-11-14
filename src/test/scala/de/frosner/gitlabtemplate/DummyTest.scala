b3d5a2ac462c3d54f589f8cf77c2f165e4c85537
Frank Rosner <frank@fam-rosner.de>
2017-11-14 14:49:30 +0100

    add docker command I use for manual testing

--- src/test/scala/de/frosner/gitlabtemplate/DummyTest.scala
+++ src/test/scala/de/frosner/gitlabtemplate/DummyTest.scala
@@ -19,6 +19,16 @@ package de.frosner.gitlabtemplate
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
+/**
+  * docker run --rm \
+    -p 443:443 \
+    -p 80:80 \
+    -p 22:22 \
+    -v $(pwd)/gitlab/config:/etc/gitlab \
+    -v $(pwd)/gitlab/logs:/var/log/gitlab \
+    -v $(pwd)/gitlab/data:/var/opt/gitlab \
+    gitlab/gitlab-ce:9.4.5-ce.0
+  */
 object DummyTest extends Properties("String") {
 
   property("substring") = forAll { (a: String, b: String, c: String) =>
