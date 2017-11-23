79732316191f3062417696f4efb4573d4fa2f4e7
Frank Rosner <frank@fam-rosner.de>
2017-11-23 14:09:18 +0100

    don't write folders for inactive users

--- src/main/scala/de/frosner/gitlabtemplate/FileSystemSink.scala
+++ src/main/scala/de/frosner/gitlabtemplate/FileSystemSink.scala
@@ -14,19 +14,19 @@ class FileSystemSink(rootDirectory: Path, publicKeysFileName: String, allowEmpty
     Try {
       val numKeysWritten = usersAndKeys.map {
         case (user, keys) =>
-          val userDir = rootDirectory.resolve(user)
-          val publicKeysFile = userDir.resolve(publicKeysFileName)
-          if (userDir.toFile.isDirectory) {
-            logger.debug(s"User directory '${userDir.toAbsolutePath}' already exists")
-          } else {
-            logger.debug(s"Creating user directory '${userDir.toAbsolutePath}'")
-          }
-          Files.createDirectories(userDir)
           if (keys.nonEmpty || allowEmpty) {
+            val userDir = rootDirectory.resolve(user)
+            val publicKeysFile = userDir.resolve(publicKeysFileName)
+            if (userDir.toFile.isDirectory) {
+              logger.debug(s"User directory '${userDir.toAbsolutePath}' already exists")
+            } else {
+              logger.debug(s"Creating user directory '${userDir.toAbsolutePath}'")
+            }
+            Files.createDirectories(userDir)
             logger.debug(s"Writing public keys to '${publicKeysFile.toAbsolutePath}'")
             Files.write(publicKeysFile, keys.mkString("\n").getBytes(StandardCharsets.UTF_8))
           } else {
-            logger.debug(s"Skipping '${publicKeysFile.toAbsolutePath}' because it would be empty")
+            logger.debug(s"Skipping '$user' because it doesn't have any keys")
           }
           keys.size
       }.sum
---
+++ src/test/scala/de/frosner/gitlabtemplate/FileSystemSinkSpec.scala
@@ -0,0 +1,45 @@
+package de.frosner.gitlabtemplate
+
+import java.nio.file.Files
+
+import org.scalatest._
+
+import scala.io.Source
+
+class FileSystemSinkSpec extends FlatSpec with Matchers {
+
+  private val testName = this.getClass.getSimpleName
+
+  private val usersAndKeys = Map(
+    "user1" -> Set("key1", "key2"),
+    "user2" -> Set("key1"),
+    "user3" -> Set.empty[String]
+  )
+
+  "A filesystem sink" should "create the folders for each users if they don't exist" in {
+    val tmpDir = Files.createTempDirectory(testName)
+    val sink = new FileSystemSink(tmpDir, "authorized_keys", true)
+    sink.write(usersAndKeys)
+    all(usersAndKeys.keys.map(user => tmpDir.resolve(user).toFile)) should exist
+  }
+
+  it should "not create the folders for users that have no keys if not desired" in {
+    val tmpDir = Files.createTempDirectory(testName)
+    val sink = new FileSystemSink(tmpDir, "authorized_keys", false)
+    sink.write(usersAndKeys)
+    tmpDir.resolve("user3").toFile shouldNot exist
+  }
+
+  it should "write the keys to the authorized keys file" in {
+    val tmpDir = Files.createTempDirectory(testName)
+    val authorizedKeysFileName = "authorized_keys"
+    val sink = new FileSystemSink(tmpDir, authorizedKeysFileName, true)
+    sink.write(usersAndKeys)
+    val writtenUsersAndKeys = usersAndKeys.keys.map { user =>
+      val file = tmpDir.resolve(user).resolve(authorizedKeysFileName).toFile
+      (user, Source.fromFile(file).getLines().toSet)
+    }.toMap
+    writtenUsersAndKeys === usersAndKeys
+  }
+
+}
